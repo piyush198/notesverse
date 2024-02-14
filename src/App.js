7187ca533d3d17390f351fc98d0b3d5a0531e2e5
divyamsharma822 <divyamsharma822@gmail.com>
2024-02-14 15:34:32 +0530

    add course data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
 import React from "react";
+import Sidebar from "./components/Sidebar";
+import SemesterList from "./components/SemesterList";
 
 function App() {
     return (
         <React.Suspense fallback={<div>Loading...</div>}>
+            <Sidebar />
             <Routes>
-                <Route index element={<Sidebar />} />
+                <Route
+                    index
+                    element={<div>NotesVerse</div>}
+                />
+                <Route
+                    path='/course/:id'
+                    element={<SemesterList />}
+                />
             </Routes>
         </React.Suspense>
     );
---
+++ src/data.js
@@ -0,0 +1,181 @@
+export const data = {
+    cs: [
+        {
+            Applied_Mathematics: [
+                {
+                    name: "Unit 1",
+                    url: "http://dsfdsf",
+                },
+                {
+                    name: "Unit 2",
+                    url: "http://dsfdsf",
+                },
+                {
+                    name: "Unit 3",
+                    url: "http://dsfdsf",
+                },
+                {
+                    name: "Unit 4",
+                    url: "http://dsfdsf",
+                },
+            ],
+
+            Applied_Physics_1: [
+                {
+                    name: "Unit 1",
+                    url: "http://dsfdsf",
+                },
+                {
+                    name: "Unit 2",
+                    url: "http://dsfdsf",
+                },
+                {
+                    name: "Unit 3",
+                    url: "http://dsfdsf",
+                },
+            ],
+            Applied_Chemistry_1: [
+                {
+                    name: "Unit 1",
+                    url: "http://dsfdsf",
+                },
+                {
+                    name: "Unit 2",
+                    url: "http://dsfdsf",
+                },
+                {
+                    name: "Unit 3",
+                    url: "http://dsfdsf",
+                },
+            ],
+            Manufacturing_Process: [
+                {
+                    name: "Unit 1",
+                    url: "http://dsfdsf",
+                },
+                {
+                    name: "Unit 2",
+                    url: "http://dsfdsf",
+                },
+                {
+                    name: "Unit 3",
+                    url: "http://dsfdsf",
+                },
+            ],
+            Introduction_To_AutoCAD: [
+                {
+                    name: "Unit 1",
+                    url: "http://dsfdsf",
+                },
+                {
+                    name: "Unit 2",
+                    url: "http://dsfdsf",
+                },
+                {
+                    name: "Unit 3",
+                    url: "http://dsfdsf",
+                },
+            ],
+            Communication_Skills: [
+                {
+                    name: "Unit 1",
+                    url: "http://dsfdsf",
+                },
+                {
+                    name: "Unit 2",
+                    url: "http://dsfdsf",
+                },
+                {
+                    name: "Unit 3",
+                    url: "http://dsfdsf",
+                },
+            ],
+        },
+        {
+            Applied_Mathematics_2: [
+                {
+                    name: "Unit 1",
+                    url: "http://dsfdsf",
+                },
+                {
+                    name: "Unit 2",
+                    url: "http://dsfdsf",
+                },
+                {
+                    name: "Unit 3",
+                    url: "http://dsfdsf",
+                },
+                {
+                    name: "Unit 4",
+                    url: "http://dsfdsf",
+                },
+            ],
+
+            Applied_Physics_2: [
+                {
+                    name: "Unit 1",
+                    url: "http://dsfdsf",
+                },
+                {
+                    name: "Unit 2",
+                    url: "http://dsfdsf",
+                },
+                {
+                    name: "Unit 3",
+                    url: "http://dsfdsf",
+                },
+            ],
+            Applied_Chemistry_2: [
+                {
+                    name: "Unit 1",
+                    url: "http://dsfdsf",
+                },
+                {
+                    name: "Unit 2",
+                    url: "http://dsfdsf",
+                },
+                {
+                    name: "Unit 3",
+                    url: "http://dsfdsf",
+                },
+            ],
+        },
+    ],
+    dbms: [
+        {
+            OS: [
+                {
+                    name: "Unit 1",
+                    url: "http://dsfdsf",
+                },
+                {
+                    name: "Unit 2",
+                    url: "http://dsfdsf",
+                },
+                {
+                    name: "Unit 3",
+                    url: "http://dsfdsf",
+                },
+                {
+                    name: "Unit 4",
+                    url: "http://dsfdsf",
+                },
+            ],
+
+            Netwokring: [
+                {
+                    name: "Unit 1",
+                    url: "http://dsfdsf",
+                },
+                {
+                    name: "Unit 2",
+                    url: "http://dsfdsf",
+                },
+                {
+                    name: "Unit 3",
+                    url: "http://dsfdsf",
+                },
+            ],
+        },
+    ],
+};
